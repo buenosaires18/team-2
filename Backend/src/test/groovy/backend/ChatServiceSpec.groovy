cc31537a47cf1d3c5380101e2b2605e5ca81e3c7
Tomás Hurrell <hurrelltomas@gmail.com>
2018-05-25 23:18:22 -0300

    Testing User and PublicationServices

--- Backend/src/test/groovy/backend/ChatServiceSpec.groovy
+++ Backend/src/test/groovy/backend/ChatServiceSpec.groovy
@@ -1,17 +1,25 @@
 package backend
 
+import grails.test.hibernate.HibernateSpec
 import grails.testing.services.ServiceUnitTest
-import spock.lang.Specification
 
-class ChatServiceSpec extends Specification implements ServiceUnitTest<ChatService>{
+class ChatServiceSpec extends HibernateSpec implements ServiceUnitTest<ChatService>{
+
+    ChatService chatService
+    Publication publication
+    User        user
 
     def setup() {
+        chatService = new ChatService()
+        user        = new User("HurrellT","Tomás Hurrell","hurrelltomas@gmail.com","pass1234","tlh11")
+        publication = new Publication("Publication","Publication introduction","Description","Pescar",user)
+
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "A chat service with"() {
         expect:"fix me"
             true == false
     }
--- Backend/src/test/groovy/backend/PublicationServiceSpec.groovy
+++ Backend/src/test/groovy/backend/PublicationServiceSpec.groovy
@@ -1,18 +1,34 @@
 package backend
 
+import grails.test.hibernate.HibernateSpec
 import grails.testing.services.ServiceUnitTest
-import spock.lang.Specification
 
-class PublicationServiceSpec extends Specification implements ServiceUnitTest<PublicationService>{
+class PublicationServiceSpec extends HibernateSpec implements ServiceUnitTest<PublicationService>{
+
+    PublicationService  publicationService
+    UserService         userService
+    Publication         publication
+    User                user
 
     def setup() {
+        user                = new User("HurrellT","Tomás Hurrell","hurrelltomas@gmail.com","pass1234","tlh11")
+        userService         = new UserService()
+        publication         = new Publication("Publication","Publication introduction","Description","Pescar",user)
+        publicationService  = new PublicationService()
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    def "A publication service can save and recover a publication"() {
+        when:
+            publicationService.savePublication(publication)
+        then:
+            def recoveredPublication = publicationService.getPublicationById(publication.id)
+            recoveredPublication.title          ==  publication.title
+            recoveredPublication.introduction   ==  publication.introduction
+            recoveredPublication.description    ==  publication.description
+            recoveredPublication.company        ==  publication.company
+            recoveredPublication.userId         ==  user.id
     }
 }
