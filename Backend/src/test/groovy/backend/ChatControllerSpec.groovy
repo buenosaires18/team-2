8e07f103e52f5b3234d0ecb3cfc09333de74e0a8
DominikowIvan <dominikowivan@gmail.com>
2018-05-25 23:19:49 -0300

    The tests for the controllers Publication and Chat where finished

--- Backend/src/test/groovy/backend/ChatControllerSpec.groovy
+++ Backend/src/test/groovy/backend/ChatControllerSpec.groovy
@@ -1,18 +1,61 @@
 package backend
 
+import grails.converters.JSON
 import grails.testing.web.controllers.ControllerUnitTest
+import groovy.json.JsonBuilder
 import spock.lang.Specification
 
 class ChatControllerSpec extends Specification implements ControllerUnitTest<ChatController> {
 
-    def setup() {
-    }
 
-    def cleanup() {
-    }
+        ChatService     mockChatService
+        def             unJsonBuilder
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
+        def setup(){
+
+            unJsonBuilder= new JsonBuilder()
+
+            mockChatService = Mock(ChatService)
+            controller.chatService      =  mockChatService
+
+        }
+
+        def 'given a correct Message the response is positive'() {
+            given:
+
+            def aMessage = unJsonBuilder {
+                user "pepita"
+                text "I like this"
+            } as JSON
+
+            request.setJSON(aMessage)
+            request.setMethod("POST")
+
+            when:
+            controller.addMessageToChatPublication()
+
+            then:
+            response.status == 200
+
+        }
+
+
+        def 'given a bad message the response is negative '() {
+            given:
+
+            def aMessage = unJsonBuilder {
+                asdasd "pepita"
+                texdasdat "I like this"
+            } as JSON
+
+            request.setJSON(aMessage)
+            request.setMethod("POST")
+
+            when:
+            controller.addMessageToChatPublication()
+
+            then:
+            response.status == 400
+        }
+
 }
--- Backend/src/test/groovy/backend/PublicationControllerSpec.groovy
+++ Backend/src/test/groovy/backend/PublicationControllerSpec.groovy
@@ -1,18 +1,65 @@
 package backend
 
+import grails.converters.JSON
 import grails.testing.web.controllers.ControllerUnitTest
+import groovy.json.JsonBuilder
 import spock.lang.Specification
 
 class PublicationControllerSpec extends Specification implements ControllerUnitTest<PublicationController> {
 
-    def setup() {
+    PublicationService     mockpublicationService
+    def             unJsonBuilder
+
+    def setup(){
+
+        unJsonBuilder= new JsonBuilder()
+
+        mockpublicationService             = Mock(PublicationService)
+        controller.publicationService      = mockpublicationService
+
     }
 
-    def cleanup() {
+    def 'given a correct Publication the response is positive'() {
+        given:
+
+        def aPublication = unJsonBuilder {
+             title        "Nuevo Trabajo"
+             introduction "Me gusto"
+             description  "Me gusto mas"
+             company      "A cara de perro"
+        } as JSON
+
+        request.setJSON(aPublication)
+        request.setMethod("POST")
+
+        when:
+        controller.savePublication()
+
+        then:
+        response.status == 200
+
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+
+    def 'given a bad publication the response is negative '() {
+        given:
+
+        def aPublication = unJsonBuilder {
+            safasfe        "Nuevo Trabajo"
+            introasfduction "Me gusto"
+            descrasfasfiption  "Me gusto mas"
+            comasfasfpany      "A cara de perro"
+            comasfwetweasfpany      "A cara de perro"
+        } as JSON
+
+        request.setJSON(aPublication)
+        request.setMethod("POST")
+
+        when:
+        controller.savePublication()
+
+        then:
+        response.status == 400
+
     }
 }
